e06b37f4b48f9df995f7eadad4dbe9e034c2ea58
Hughes, Joseph D <jdhughes@usgs.gov>
2021-03-13 10:55:04 -0600

    update IMS *_hclose to *_dvclose

---
+++ mf6/python/update_ims.py
@@ -0,0 +1,26 @@
+import os
+
+ims_pths = []
+for root, dirs, files in os.walk(".."):
+   for name in files:
+       if name.lower().endswith(".ims"):
+           pth = os.path.join(root, name)
+           ims_pths.append(pth)
+
+replace_dict = {
+    "inner_hclose": "inner_dvclose",
+    "outer_hclose": "outer_dvclose",
+
+}
+
+for idx, pth in enumerate(ims_pths):
+    print("{:3d}: {}".format(idx + 1, pth))
+    with open(pth) as f:
+        lines = f.read().splitlines()
+
+        f = open(pth, "w")
+        for line in lines:
+            for text0, text1 in replace_dict.items():
+                line = line.lower().replace(text0, text1)
+            f.write("{}\n".format(line))
+        f.close()
